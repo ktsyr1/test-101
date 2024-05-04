3d32fe20604e9349392d4df97ab01f134acf1e9b
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-05-05 01:02:37 +0300

    Update menu.tsx

--- src/component/theme/menu.tsx
+++ src/component/theme/menu.tsx
@@ -1,65 +1,62 @@
-"use client"
+// "use client"
 import Image from "next/image";
 import { IconClose, IconHome, IconLogo, IconMenu, IconPlay } from "../icons";
-import { useState } from "react";
+// import { useState } from "react";
 import Link from "next/link";
 import { loaderProp } from "../lib";
 
-export default function Menu() {
-    let [open, setOpen] = useState(false)
-    let Icon = open ? IconClose : IconMenu
-    const handleOpen = () => {
-        setOpen(!open)
-    }
-    let list = [
-        {
-            title: "عن الشركة",
-            to: "/about-us",
-            Icon: "company.png"
-        }, {
-            title: "خدماتنا",
-            to: "/services",
-            Icon: "services.png"
-        }, {
-            title: "المدونة ",
-            to: "/blog",
-            Icon: "blog.png"
-        }, {
-            title: "فرصة عمل",
-            to: "/jobs",
-            Icon: "job.png"
-        }, {
-            title: "إنضم إلى فريقنا",
-            to: "/join",
-            Icon: "join.png"
-        }, {
-            title: "حول التطبيق",
-            to: "/about-app",
-            Icon: "about-app.png"
-        }, {
-            title: "الاسئلة الشائعة",
-            to: "/FAQ",
-            Icon: "about-app.png"
-        }
-    ]
+export default function Menu() { 
 
     return (
         <div className="flex flex-col fixed z-[60] top-0" >
             {/* menu */}
-            <div onClick={handleOpen}>
-                <Icon className={' tap:w-[75px] w-[55px] tap:h-[75px] h-[55px] hover:mr-1 '} />
-            </div>
-            <div className={`fixed flex flex-col tap:top-[78px] top-[55px] w-full bg-gradient-to-r from-[#0043cecc] to-[#032da6cc] h-full ${!open ? "!hidden" : " "}`} >
+
+            <input type="checkbox" id="a" value="a" name="openMenu" className="peer opacity-0 p-10 border-2 absolute tap:w-[75px] w-[55px] tap:h-[75px] h-[55px] cursor-pointer hover:border-2 hover:border-safety-700" required />
+            <label htmlFor="a" className="peer-checked:hidden flex">
+                <div  >
+                    <IconMenu className={' tap:w-[75px] w-[55px] tap:h-[75px] h-[55px] hover:mr-1 '} />
+                </div>
+            </label>
+            <label htmlFor="a" className="peer-checked:flex hidden">
+                <div  >
+                    <IconClose className={' tap:w-[75px] w-[55px] tap:h-[75px] h-[55px] hover:mr-1 '} />
+                </div>
+            </label>
+            <label htmlFor="a" className={`fixed   flex-col tap:top-[78px] top-[55px] w-full bg-gradient-to-r from-[#0043cecc] to-[#032da6cc] h-full peer-checked:flex hidden`} >
                 <div className="h-full lap:w-[75px] w-[55px] bg-white fixed"> </div>
-                <div className="h-70 w-full fixed z-20 top-[100px] lap:top-[200px] " onClick={handleOpen}>
+                <div className="h-70 w-full fixed z-20 top-[100px] lap:top-[200px] " >
                     {list.map(a => <Row {...a} key={a.title} />)}
                 </div>
 
-            </div>
+            </label>
 
         </div >
     )
 }
+
+// export function Menu2() {
+//     let [open, setOpen] = useState(false)
+//     let Icon = open ? IconClose : IconMenu
+//     const handleOpen = () => {
+//         setOpen(!open)
+//     }
+//     return (
+//         <div className="flex flex-col fixed z-[60] top-0" >
+//             {/* menu */}
+//             <div onClick={handleOpen}>
+//                 <Icon className={' tap:w-[75px] w-[55px] tap:h-[75px] h-[55px] hover:mr-1 '} />
+//             </div>
+//             <div className={`fixed flex flex-col tap:top-[78px] top-[55px] w-full bg-gradient-to-r from-[#0043cecc] to-[#032da6cc] h-full ${!open ? "!hidden" : " "}`} >
+//                 <div className="h-full lap:w-[75px] w-[55px] bg-white fixed"> </div>
+//                 <div className="h-70 w-full fixed z-20 top-[100px] lap:top-[200px] " onClick={handleOpen}>
+//                     {list.map(a => <Row {...a} key={a.title} />)}
+//                 </div>
+
+//             </div>
+
+//         </div >
+//     )
+// }
 type Row = { Icon: any; title: string; to: string, }
 
 function Row({ title, Icon, to, }: Row) {
@@ -73,3 +70,35 @@ function Row({ title, Icon, to, }: Row) {
         </Link>
     )
 }
+
+let list = [
+    {
+        title: "عن الشركة",
+        to: "/about-us",
+        Icon: "company.png"
+    }, {
+        title: "خدماتنا",
+        to: "/services",
+        Icon: "services.png"
+    }, {
+        title: "المدونة ",
+        to: "/blog",
+        Icon: "blog.png"
+    }, {
+        title: "فرصة عمل",
+        to: "/jobs",
+        Icon: "job.png"
+    }, {
+        title: "إنضم إلى فريقنا",
+        to: "/join",
+        Icon: "join.png"
+    }, {
+        title: "حول التطبيق",
+        to: "/about-app",
+        Icon: "about-app.png"
+    }, {
+        title: "الاسئلة الشائعة",
+        to: "/FAQ",
+        Icon: "about-app.png"
+    }
+]
