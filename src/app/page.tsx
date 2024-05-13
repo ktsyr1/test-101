9f3803cb5c51a62f33d8dcc4e0cb3d7d3feba72e
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-05-13 12:40:13 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <JobsPart />
             <BlogPart />
             <OurClientsTestimonials />
-            <Consultation />
+            {/* <Consultation /> */}
         </div>
     );
 }
