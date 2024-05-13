4d4463f90f36cf827a750485a0c18c8864f0713b
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-05-13 21:49:59 +0300

    Update page.tsx

--- src/app/(eng)/get-service/payment/page.tsx
+++ src/app/(eng)/get-service/payment/page.tsx
@@ -24,6 +24,7 @@ export default async function GetService({ searchParams: { id } }: any) {
     return (
         <div className="flex flex-col justify-center items-center min-h-[600px]">
             {data.code == 400 && <p className="font-medium text-red-600 text-xl"> يؤسفنا إبلاغك بأن عملية الدفع لم تكن ناجحة.</p>}
+            {data.code == 200 && <p className="font-medium text-green-500 text-xl"> {data?.messages}</p>}
 
         </div>
     )
