9cf06b6738557c1c11ff667b004b812d0d5be5b6
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-05-28 16:38:03 +0300

    Update page.tsx

--- src/app/(client)/services/get/page.tsx
+++ src/app/(client)/services/get/page.tsx
@@ -13,15 +13,13 @@ export default function GetServices() {
                 <div className="mt-4 w-full m-auto bg-white">
                     <div className=" flex items-center w-full max-w-[1360px]  m-auto   flex-col">
                         <div className="flex flex-col items-center m-auto w-[90%] my-20 " >
-                            <div className="flex tap:flex-row flex-col justify-between w-full mb-8 items-center" > 
+                            <div className="flex tap:flex-row flex-col justify-between w-full mb-8 items-center" >
                                 <h2 className="w-full text-start lap:text-5xl tap:text-4xl text-3xl my-4 font-bold text-safety-700 mr-10">طلب الخدمة </h2>
                                 <CheckCountService />
                             </div>
                         </div>
                     </div>
-
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScedu-uK2yI5xFeTB0zgDCIwZCR9MVa9ygOkPhUSMGVzJ6tcA/viewform?embedded=true" className="m-auto border-0 max-w-[640px] w-full" height="800"    >جارٍ التحميل…</iframe>
-
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe-Wux5xwL5smqGWTO21HqxqqMT-4jZNU78Ysw5ev0uXaImzw/viewform?embedded=true" className="m-auto border-0 max-w-[640px] w-full" height="800" >جارٍ التحميل…</iframe>
                 </div>
             </div>
         </div>
