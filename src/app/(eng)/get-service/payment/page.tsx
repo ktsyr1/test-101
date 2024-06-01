fa8c823377a140f0e6b7a69dcbd8061af8b75e9b
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-06-01 22:16:18 +0300

    Update page.tsx

--- src/app/(eng)/get-service/payment/page.tsx
+++ src/app/(eng)/get-service/payment/page.tsx
@@ -1,4 +1,5 @@
 "use server"
+import GetFatch from "@/component/froms/service/sendpaymonet";
 import axios from "axios";
 import { cookies } from 'next/headers'
 
@@ -8,12 +9,11 @@ export default async function GetService({ searchParams: { id } }: any) {
     let userToken: any = store.get("userToken")
     if (userToken?.value) {
         let tran_ref: any = store.get("tran_ref")
-        let { data } = await axios.get(`${process.env.NEXT_PUBLIC_apis}/payment/query?id=${id}&userToken=${userToken?.value}&tran_ref=${tran_ref?.value}`)
-
+        let url = `/payment/query?id=${id}&userToken=${userToken?.value}&tran_ref=${tran_ref?.value}`
+        let data = await GetFatch(url)
         return (
             <div className="flex flex-col justify-center items-center min-h-[600px]">
-                {data.code == 400 && <p className="font-medium text-red-600 text-xl"> يؤسفنا إبلاغك بأن عملية الدفع لم تكن ناجحة.</p>}
-                {data.code == 200 && <p className="font-medium text-green-500 text-xl"> {data?.messages}</p>}
+                <View data={data} />
             </div>
         )
     } else return (
@@ -21,4 +21,14 @@ export default async function GetService({ searchParams: { id } }: any) {
             <p className="font-medium text-red-600 text-xl">لم تقم بتسجيل الدخول</p>
         </div>
     )
+}
+
+function View({ data }: any) {
+    if (data.code == 400) return <p className="font-medium text-red-600 text-xl"> يؤسفنا إبلاغك بأن عملية الدفع لم تكن ناجحة.</p>
+    if (data.code == 200) {
+        if (data.data.payment_result.response_message == 'Cancelled') {
+            return <p className="font-medium text-red-600 text-xl"> عملية الدفع تم الغائها.</p>
+        } else return <p className="font-medium text-green-500 text-xl">  تم اتمام عملية الدفع بنجاح </p>
+
+    }
 }
