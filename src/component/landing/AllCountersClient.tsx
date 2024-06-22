cc735cba428a3289c967bef1017f9f57c89937c2
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-06-22 08:33:47 +0300

    add

--- src/component/landing/AllCountersClient.tsx
+++ src/component/landing/AllCountersClient.tsx
@@ -5,7 +5,7 @@ const inter: any = localFont({ src: './DS_Digital.ttf', weight: '100', style: 'n
 export async function Counter({ name, conter, Default }: any) {
     return (
         <div className="md:mx-4 flex justify-center flex-col m-auto ">
-            <img src={`/images/counter-${name}.png`} alt="counter img" className=" min-[1010px]:w-56 w-36 max-[450px]:w-36 -lap:h-56  " />
+            <img src={`/images/counter-${name}.png`} alt="counter img" className=" min-[1010px]:w-56 w-36 max-[450px]:w-36 " />
             <div className={inter.className + " w-auto"}>
                 <div className={`m-auto min-[1010px]:mt-[-165px] tap:mt-[-105px] min-[450px]:mt-[-105px] mt-[-90px] w-max min-[1010px]:*:text-3xl *:text-xl `}>
                     <p className={' text-slate-700 w-full  '}>{Default}</p>
