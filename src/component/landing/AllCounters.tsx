5a7a51d2e65708b2e5596ee657d8b6986574a801
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-06-22 23:49:01 +0300

    update

--- src/component/landing/AllCounters.tsx
+++ src/component/landing/AllCounters.tsx
@@ -11,9 +11,7 @@ async function GetData(): Promise<any> {
     let Cookie = `${all.map(a => `${a.name}=${a.value}; `)}`.replaceAll(" ,", " ")
     let headers: any = { "Content-Type": "application/json", "Accept-Language": "ar-SA", "Cookie": Cookie }
     return axios.get(`${process.env.NEXT_PUBLIC_API}/Guest/Counters`, { headers })
-        .then(({ data }) => {
-            return data
-        })
+        .then(({ data }) => { return data })
         .catch((error: Function) => console.log(error))
 }
 export default async function AllCounters() {
@@ -24,9 +22,9 @@ export default async function AllCounters() {
         <div className="  w-full bg-[#F0F0F0] flex justify-center ">
             <div className=" max-w-[1000px] w-full bg-[#F0F0F0]  ">
                 <div className="flex flex-row items-center lap:max-w-[1000px] tap:w-[80%] justify-between my-20 select-none px-3 m-auto">
-                    <Counter names="عميل" name="client" conter={data?.clientsCounter} Default={"0000"} />
-                    <Counter names="تقرير" name="report" conter={data?.assessmentsCounter} Default={"0000"} />
-                    <Counter names="زائر" name="visit" conter={data?.visitsCounter} Default={"000000"} />
+                    <Counter names="عميل" name="client" conter={data?.clientsCounter || 2221} Default={"0000"} />
+                    <Counter names="تقرير" name="report" conter={data?.assessmentsCounter || 23321} Default={"0000"} />
+                    <Counter names="زائر" name="visit" conter={data?.visitsCounter || 122611} Default={"00000000"} />
                 </div>
             </div>
         </div>
--- src/component/landing/AllCountersClient.tsx
+++ src/component/landing/AllCountersClient.tsx
@@ -9,13 +9,13 @@ export async function Counter({ names, conter, Default }: any) {
             <div className="md:mx-4 flex justify-center flex-col m-auto ">
                 <img src={`/images/counter.png`} alt="counter img" className=" min-[1010px]:w-56 min-[450px]:w-36 w-28 " />
                 <div className={inter.className + " absolute flex items-center justify-center text-center m-auto min-[1010px]:w-48 w-28 min-[450px]:w-36 min-[1010px]:h-56 h-36 max-[450px]:h-36"}>
-                    <div className={`m-auto w-max min-[1010px]:*:text-5xl min-[450px]:*:text-3xl  *:text-xl `}>
+                    <div className={`m-auto w-max min-[1010px]:*:text-4xl min-[450px]:*:text-3xl  *:text-xl `}>
                         <p className={' text-slate-700 w-full '}>{Default.slice(0, conter.toString().length)}</p>
-                        <p className={'  min-[1010px]:mt-[-48px] min-[450px]:mt-[-36px] mt-[-28px] text-white w-full '}>{conter}</p>
+                        <p className={'  min-[1010px]:mt-[-40px] min-[450px]:mt-[-36px] mt-[-28px] text-white w-full '}>{conter}</p>
                     </div>
                 </div>
             </div>
-            <p className="font-bold lap:text-4xl text-[#0043ce] text-center tap:text-xl text-sm my-4" >{names}</p>
+            <p className="font-bold lap:text-4xl tap:text-2xl text-lg text-[#0043ce] text-center  my-4" >{names}</p>
         </div>
     )
 }
