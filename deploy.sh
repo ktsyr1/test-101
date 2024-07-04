4de24b61d4dc192985e710dec835fcdd5069ca15
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-07-04 16:45:17 +0300

    aa

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# Remove the old directory
-rm -rf /c/inspectex/inspectex/test-101
-echo "تم تنضيف الملفات "
 
 # Clone the repository
-git clone https://github.com/ktsyr1/test-101.git
+git pull
 
 echo "تم تنزيل الملفات"
-# Move files
-mv /c/inspectex/inspectex/test-101/* /c/inspectex/inspectex/
-
-echo "تم استخراج الملفات "
 # Wait for 30 seconds
 # sleep 5
+pm2 stop app 
+npm run build 
+pm2 start app 
 
 # Remove the old directory
 # rm -rf /c/inspectex/inspectex/test-101
 
 echo "تم نشر التحديث  ❤😎 "
-# Wait for 30 seconds
-sleep 10
+# Wait for 30 seconds 
